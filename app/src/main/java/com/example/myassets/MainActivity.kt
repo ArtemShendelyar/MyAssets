a011c202d3a61e335e8d7b888d91ac6315fab3da
belgoscode <tima@darida.net>
2024-07-07 18:14:28 +0300

    Добавлены "заглушки" для фрагментов, добавлено нижнее меню через ресурс и его функциональность.

--- app/src/main/java/com/example/myassets/MainActivity.kt
+++ app/src/main/java/com/example/myassets/MainActivity.kt
@@ -3,10 +3,34 @@ package com.example.myassets
 import android.os.Bundle
 
 import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.Fragment
+import com.example.myassets.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding : ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        replaceFragment(HomeFragment())
+
+        binding.navigationHolder.setOnItemSelectedListener {
+            when(it.itemId){
+                R.id.nav_bar_home -> replaceFragment(HomeFragment())
+                R.id.nav_bar_portfolios -> replaceFragment(PortfolioListFragment())
+                R.id.nav_bar_assets -> replaceFragment(AssetsListFragment())
+                R.id.nav_bar_settings -> replaceFragment(SettingsFragment())
+                else -> {}
+            }
+            true
+        }
+    }
+
+    private fun replaceFragment(fragment : Fragment) {
+        val fragmentManager = supportFragmentManager
+        val fragmentTransaction = fragmentManager.beginTransaction()
+        fragmentTransaction.replace(R.id.fragment_holder,fragment)
+        fragmentTransaction.commit()
     }
 }
