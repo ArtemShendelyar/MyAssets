329e6b7086903c01ef603e476e626914069d2da6
ArtemShendelyar <artemshendelyar@gmail.com>
2024-07-10 14:50:03 +0300

    Init architecture

--- app/src/main/java/com/example/myassets/presentation/MainActivity.kt
+++ app/src/main/java/com/example/myassets/presentation/MainActivity.kt
@@ -1,7 +1,8 @@
-package com.example.myassets
+package com.example.myassets.presentation
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import com.example.myassets.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
