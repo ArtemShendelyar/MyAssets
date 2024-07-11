b4fbe6b6b5b790c375b7c844606902db425a181b
ArtemShendelyar <artemshendelyar@gmail.com>
2024-07-11 13:15:20 +0300

    setupWithNavController()

--- app/src/main/java/com/example/myassets/presentation/ui/MainActivity.kt
+++ app/src/main/java/com/example/myassets/presentation/ui/MainActivity.kt
@@ -3,9 +3,8 @@ package com.example.myassets.presentation.ui
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.fragment.NavHostFragment
-import com.example.myassets.R
+import androidx.navigation.ui.setupWithNavController
 import com.example.myassets.databinding.ActivityMainBinding
-import com.example.myassets.presentation.ui.home.HomeFragmentDirections
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
@@ -14,28 +13,9 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        val navHostFragment =
-            supportFragmentManager.findFragmentById(R.id.fragment_holder) as NavHostFragment
+        val navHostFragment = binding.fragmentHolder.getFragment() as NavHostFragment
         val navController = navHostFragment.navController
 
-        binding.navigationBar.setOnItemSelectedListener {
-            when (it.itemId) {
-                R.id.nav_bar_home -> navController.popBackStack()
-                R.id.nav_bar_portfolios -> navController.navigate(
-                    HomeFragmentDirections.actionHomeFragmentToPortfolioListFragment()
-                )
-
-                R.id.nav_bar_assets -> navController.navigate(
-                    HomeFragmentDirections.actionHomeFragmentToAssetListFragment()
-                )
-
-                R.id.nav_bar_settings -> navController.navigate(
-                    HomeFragmentDirections.actionHomeFragmentToSettingsFragment()
-                )
-
-                else -> {}
-            }
-            true
-        }
+        binding.navigationBar.setupWithNavController(navController)
     }
 }
