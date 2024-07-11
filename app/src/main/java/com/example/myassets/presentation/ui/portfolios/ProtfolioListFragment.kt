a6cacf320744af7c97a22df7e4a8f2017c5d420a
ArtemShendelyar <artemshendelyar@gmail.com>
2024-07-11 18:28:03 +0300

    Правки

--- app/src/main/java/com/example/myassets/presentation/ui/portfolios/ProtfolioListFragment.kt
+++ app/src/main/java/com/example/myassets/presentation/ui/portfolios/ProtfolioListFragment.kt
@@ -5,14 +5,23 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
-import com.example.myassets.R
+import com.example.myassets.databinding.FragmentProtfolioListBinding
 
 class ProtfolioListFragment : Fragment() {
+    private var _binding: FragmentProtfolioListBinding? = null
+    private val binding get() = _binding!!
+
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        return inflater.inflate(R.layout.fragment_protfolio_list, container, false)
+    ): View {
+        _binding = FragmentProtfolioListBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
     }
 }
