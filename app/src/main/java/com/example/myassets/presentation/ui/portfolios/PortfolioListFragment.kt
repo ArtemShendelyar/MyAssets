2b845316ac052484bc49fb18bb86f4348d320db0
belgoscode <tima@darida.net>
2024-07-28 17:43:21 +0300

    Исправление опечаток.

--- app/src/main/java/com/example/myassets/presentation/ui/portfolios/PortfolioListFragment.kt
+++ app/src/main/java/com/example/myassets/presentation/ui/portfolios/PortfolioListFragment.kt
@@ -2,14 +2,14 @@ package com.example.myassets.presentation.ui.portfolios
 
 import android.view.LayoutInflater
 import android.view.ViewGroup
-import com.example.myassets.databinding.FragmentProtfolioListBinding
+import com.example.myassets.databinding.FragmentPortfolioListBinding
 import com.example.myassets.presentation.util.BasicFragment
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
-class ProtfolioListFragment : BasicFragment<FragmentProtfolioListBinding>() {
+class PortfolioListFragment : BasicFragment<FragmentPortfolioListBinding>() {
     override fun inflateViewBinding(
         inflater: LayoutInflater,
         container: ViewGroup?
-    ) = FragmentProtfolioListBinding.inflate(inflater, container, false)
+    ) = FragmentPortfolioListBinding.inflate(inflater, container, false)
 }
