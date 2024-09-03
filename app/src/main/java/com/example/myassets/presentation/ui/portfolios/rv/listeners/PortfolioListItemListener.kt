b9d5d4574536c98e5e96fee9e80dc633f9b8f882
ArtemShendelyar <artemshendelyar@gmail.com>
2024-09-03 21:47:37 +0300

    Реорганизация папок

--- app/src/main/java/com/example/myassets/presentation/ui/portfolios/rv/listeners/PortfolioListItemListener.kt
+++
@@ -1,6 +0,0 @@
-package com.example.myassets.presentation.ui.portfolios.rv.listeners
-
-interface PortfolioListItemListener {
-    fun onItemClick(itemId: Int)
-    fun onSelect(itemId: Int)
-}
---
+++ app/src/main/java/presentation/ui/home/HomeFragment.kt
@@ -0,0 +1,34 @@
+package presentation.ui.home
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.viewModels
+import com.example.myassets.databinding.FragmentHomeBinding
+import presentation.util.BasicFragment
+import dagger.hilt.android.AndroidEntryPoint
+
+@AndroidEntryPoint
+class HomeFragment : BasicFragment<FragmentHomeBinding>() {
+    private val viewModel: HomeFragmentViewModel by viewModels()
+    override fun inflateViewBinding(
+        inflater: LayoutInflater,
+        container: ViewGroup?
+    ) = FragmentHomeBinding.inflate(inflater, container, false)
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        viewModel.fetchStats()
+        viewModel.portfoliosCount.observe(viewLifecycleOwner) { count ->
+            with(binding) {
+                textPortfoliosCount.text = count.toString()
+            }
+        }
+        viewModel.assetsCount.observe(viewLifecycleOwner) { count ->
+            with(binding) {
+                textAssetsCount.text = count.toString()
+            }
+        }
+    }
+}
